fb328daf2d1444a2ad3698045c9302f86f4b2d1c
Dylan Johnson <marcusdylanjohnson@gmail.com>
2021-01-13 13:14:24 -0600

    Set up linting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parserOptions: {
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: "module", // Allows using import/export statements
+    ecmaFeatures: {},
+  },
+  settings: {},
+  env: {
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true, // Enables Node.js global variables and Node.js scoping.
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended", // Make this the last element so prettier config overrides other formatting rules
+  ],
+  rules: {
+    "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+  },
+};
